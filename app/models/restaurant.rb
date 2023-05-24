cfef9f442d61d0df9639f0f7b3831b703e992811
Augusto Lopes <meleu.dev@gmail.com>
2023-05-24 14:35:17 -0300

    required fields

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,6 @@ class Restaurant < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
-  validates :name, presence: true
-  # validates :name, :address, :category, presence: true
+  validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
 end
