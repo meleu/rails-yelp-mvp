42c46e0b252218b498136d2bc5bc8d8407adfab5
Augusto Lopes <meleu.dev@gmail.com>
2023-05-24 12:32:26 -0300

    add faker and a seed for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning the database...'
+Restaurant.destroy_all
+
+puts 'Generating restaurants...'
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
